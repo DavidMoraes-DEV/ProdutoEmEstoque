416f527f61e81491525c8f278b505fca0cd77f81
David Moraes <investtraderrs@gmail.com>
2020-09-16 18:14:48 -0300

    Classes Objetos e Atributos

---
+++ ProdutoEstoque/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace ProdutoEstoque
+{
+    class Produto
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public string Nome;
+        public double Preco;
+        public int Qtd;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Qtd * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Qtd += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Qtd -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CI)
+                + ", "
+                + Qtd
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CI);
+        }
+
+        
+
+
+
+    }
+}
---
+++ ProdutoEstoque/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutoEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Produto p;
+            p = new Produto();
+
+            Console.WriteLine("Digite os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Quantidade no estoque: ");
+            p.Qtd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p );
+            Console.WriteLine();
+
+            Console.Write("Digite o numero de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o numero de produtos a ser removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+            Console.WriteLine();
+        }
+    }
+}
+/*Fazer um programa para ler os dados de um produto em estoque (nome, preço e
+quantidade no estoque). Em seguida:
+• Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no
+estoque)
+• Realizar uma entrada no estoque e mostrar novamente os dados do produto
+• Realizar uma saída no estoque e mostrar novamente os dados do produto
+Para resolver este problema, você deve criar
+uma CLASSE conforme projeto ao lado:
+
+EXEMPLO:
+
+Entre os dados do produto:
+Nome: TV
+Preço: 900.00
+Quantidade no estoque: 10
+Dados do produto: TV, $ 900.00, 10 unidades, Total: $ 9000.00
+Digite o número de produtos a ser adicionado ao estoque: 5
+Dados atualizados: TV, $ 900.00, 15 unidades, Total: $ 13500.00
+Digite o número de produtos a ser removido do estoque: 3
+Dados atualizados: TV, $ 900.00, 12 unidades, Total: $ 10800.00
+ */
